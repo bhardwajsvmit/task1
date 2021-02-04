e64d744339977717be22f174e01895cd1812875b
Sumit Bhardwaj <bhardwajsvmit>
2021-02-04 18:08:33 +0530

    new code

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React,{useState} from 'react';
+import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import Section from './Section';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [inp,setInp] = useState('')
+  const [list,setList] =useState([]);
+
+  const addSection=()=>{
+      if(inp){
+      setList([ ...list, { key : Math.random().toString() , data:inp } ]);
+      setInp('')
+      }
+      else{
+          setList([...list])
+      }
+  }
+
+  const delSection=(itemkey)=>{
+    var newlist = list.filter(item=> item.key!=itemkey)
+    setList(newlist);
+  }
+
+
+  return (<SafeAreaView style={styles.container} >
+    <View style={{width:'100%',backgroundColor:'#66B2FF',}} >
+    <Text style={{fontSize:24,textTransform:'capitalize',padding:10,fontWeight:'700',textAlign:'center'}} >curriculum app</Text>
     </View>
-  );
+    <View style={{flexDirection:'row', justifyContent:'space-evenly',width:"100%",padding:5}} >
+    <TextInput style={{borderColor:'red',borderWidth:1,width:"50%",textAlign:'center',fontSize:17}} value={inp} onChangeText={text=>setInp(text)} />
+    <Button title="Add Section" onPress={addSection} />
+    </View>
+    <ScrollView>
+      {list.map((item)=>
+      <View key={item.key}>
+        <View style={{flexDirection:'row', justifyContent:'space-evenly',width:"100%",backgroundColor:'yellow'}} >
+        <Text style={styles.sectionheading} >SECTION : {item.data}</Text>
+        <Button title="Delete" onPress={()=>delSection(item.key)} />
+        
+        </View>
+        <Section/>
+      </View>
+      )}
+    </ScrollView>
+  </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +54,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  sectionheading:{
+    fontSize:20,
+    fontWeight:'bold',
+    textAlign:'center',
+    padding:5,
+   
+
+    
   },
 });
--- Section.js
+++ Section.js
@@ -1,53 +1,92 @@
-import React from 'react'
-import { View, Text, Button ,StyleSheet, TextInput } from 'react-native'
+import React,{useState} from 'react'
+import { SafeAreaView,View, Text, Button ,StyleSheet, TextInput, ScrollView, TouchableOpacity } from 'react-native'
 
-const addSection=()=>{
-    const [inp,setInp]=useState('')
-
-    return(
-        <View style={styles.lecture}>
-            <TextInput onChangeText={(text)=>setInp(text)} value={inp} />
-            <Button title="add section" />
-        </View>
-    )
-}
 
 export default function Section() {
+
+    const [inp,setInp] = useState('')
+    const [list,setList] =useState([]);
+
+    const addLecture=()=>{
+        if(inp){
+        setList([ ...list, { key : Math.random().toString() , data:inp } ]);
+        setInp('')
+        }
+        else{
+            setList([...list])
+        }
+    }
+
+    const removeLecture=(itemkey)=>{
+        var newlist = list.filter(item=> item.key!=itemkey);
+        setList(newlist);
+    }
+
     return (
-        <View style={styles.section} >
+        <SafeAreaView style={styles.section}  >
+           
             <View style={styles.sectionTab} >
-            <TextInput onChangeText={(text)=>setInp(text)} value={inp} />
-            <Text>{inp}</Text>
-            <Button tittle="add section" onPress={Section}  />
-            <Button title="add lecture" onPress={addSection} />
+            <TextInput 
+            style={{width:200,color:'red', textAlign:'center',fontSize:18,borderColor:'red',borderWidth:1,height:37}} 
+            onChangeText={(text)=>setInp(text)} 
+            value={inp} 
+            placeholder="enter lecture name" />
+            <Button title="add lecture" onPress={addLecture} />
             </View>
-        </View>
+           
+            <ScrollView > 
+                {list.map((item)=>
+                 <View key={item.key} style={styles.scrollviewitem} >
+                 <Text style={styles.scrollviewtext} >{item.data}</Text>
+                 <Button style={{flex:1,justifyContent:'center'}} title="Delete" onPress={()=>removeLecture(item.key)} />
+                 </View>
+                
+                    )}
+
+            </ScrollView>
+            
+            
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     section:{
         flex:1,
-        flexDirection:'column',
-        backgroundColor:'green',
+        backgroundColor:'white',
         justifyContent:'center',
         alignItems:'center'
 
     },
-    sectionTab : {
-      flex: 1,
-      backgroundColor: 'gray',
-      flexDirection:'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    lecture:{
+    
         sectionTab : {
-            flex: 1,
+            width:'100%',
+            height:50,
+            borderWidth:1,
+            borderColor:'gray',
             backgroundColor: 'pink',
             flexDirection:'row',
             alignItems: 'center',
-            justifyContent: 'center',
+            justifyContent: 'space-evenly',
+    },
+    scrollviewitem:{
+        backgroundColor :'orange',
+        flexDirection:'row',
+        margin:2,
+        width:'80%',
+        borderRadius:10,
+        padding:8,
+        justifyContent:'space-around',
+        alignItems:'center',
+        alignSelf:'center',
+
+    },
+    scrollviewtext:{
+        flex:4,
+        justifyContent:'center',
+        alignSelf:'center',
+        fontSize:17,
+
     },
   });
   
